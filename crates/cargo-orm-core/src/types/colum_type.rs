8f730d931cd976243c3235986a300d7168b6d3ce
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-02-19 21:37:15 +0100

    feat: basic scheme generator

---
+++ crates/cargo-orm-core/src/types/colum_type.rs
@@ -0,0 +1,51 @@
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SqlType{
+    Integer,
+    Varchar(usize),
+    Text,
+    Boolean,
+    Date,
+    Timestamp,
+}
+
+
+
+pub trait ToSqlType{
+    fn to_sql_type(&self)->SqlType;
+}
+
+impl ToSqlType for String{
+    fn to_sql_type(&self) -> SqlType {
+        if self.len()> 255 {
+            return SqlType::Text;
+        }
+        SqlType::Varchar(255)
+    }
+}
+impl ToSqlType for i32{
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for bool{
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Boolean
+    }
+}
+impl ToSqlType for chrono::NaiveDate{
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Date
+    }
+}
+impl ToSqlType for chrono::NaiveDateTime{
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Timestamp
+    }
+}
+
+impl<T: ToSqlType + Default> ToSqlType for Option<T> {
+    fn to_sql_type(&self) -> SqlType {
+        T::default().to_sql_type()
+    }
+}
---
+++ crates/cargo-orm-macros/src/generator/schema_generate.rs
@@ -0,0 +1,35 @@
+use proc_macro2::TokenStream;
+
+use crate::TableData;
+use quote::quote;
+
+pub fn generate_schema_impl(table: &TableData)->TokenStream{
+    let mut fields:Vec<TokenStream> = Vec::new();
+    for field in table.fields.iter() {
+        let field_name = &field.name;
+        let field_type = &field.ty;
+        let is_primary_key = field.is_primary_key;
+        let is_nullable = field.is_nullable;
+        fields.push(quote!{
+            cargo_orm_core::schema::table::ColumnSchemaModel::new::<#field_type>(String::from(#field_name),#is_primary_key,#is_nullable,<#field_type>::default())
+        });
+    }
+    
+    let table_name = &table.name;
+    let struct_ident = &table.ident; 
+    quote!{
+        impl cargo_orm_core::schema::table::TableSchema for #struct_ident{
+            fn get_table_name()->&'static str{
+                #table_name
+            }
+            fn get_schema()->cargo_orm_core::schema::table::TableSchemaModel{
+                cargo_orm_core::schema::table::TableSchemaModel{
+                    name: String::from(#table_name),
+                    fields: vec!(#(#fields),*),
+                    indexes: Vec::new()
+                }
+            }
+        }
+    }
+}
+
