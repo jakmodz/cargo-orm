b82902193602a5e93c38b1de0bf772623840acab
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-02-26 23:21:23 +0100

    docs: documentation for Model derive macro and all attributes of this macro

--- crates/cargo-orm-macros/src/lib.rs
+++ crates/cargo-orm-macros/src/lib.rs
@@ -7,7 +7,62 @@ use model::parse_model;
 use proc_macro::TokenStream;
 use syn::{DeriveInput, parse_macro_input};
 
-#[proc_macro_derive(Model, attributes(table, Column, PrimaryKey))]
+/// Derive macro that generates ORM boilerplate for database entity.
+/// 
+/// Annotate a struct with '#[derive(Model)]' to automaticly implement all needed traits
+/// 
+/// # Container Attributes
+/// 
+/// ### Placed at top of struct
+/// 
+/// '#[Table]'
+/// possible attributes=> name: String
+/// 
+/// ### Example 
+/// ```
+/// #[derive(Model)]
+/// #[Table(name = "users")]
+/// struct User;
+/// ```
+/// 
+/// ### Placed above field of the struct
+/// 
+/// '#[Column]'
+///  possible attributes=> name: String,unique: bool,nullable: bool
+/// 
+/// # Example
+/// ```
+/// #[Model]
+/// struct User {
+///     #[Column(name= "nm",unique = true,nullable = false)]   
+///     name: String
+///}
+/// ```
+/// 
+/// '#[PrimaryKey]'
+/// possible attributes=> generation_strategy: GenerationStrategy
+/// # Example
+/// ```
+/// #[Model]
+/// struct User {
+///     #[Column(name= "nm",unique = true,nullable = false)]   
+///     #[PrimaryKey(generation_strategy = GenerationStrategy::AutoIncrement)]
+///     id: i64
+///}
+/// ```
+/// # Examples
+/// ```
+/// #[Model]
+/// #[Table(name = "users")]
+/// struct User {
+///     #[Column(name= "id",unique = true,nullable = false)]   
+///     #[PrimaryKey(generation_strategy = GenerationStrategy::AutoIncrement)]
+///     id: i64
+///     #[Column(name= "nm",unique = true,nullable = false)]   
+///     name: String
+///}
+/// ```
+#[proc_macro_derive(Model, attributes(Table, Column, PrimaryKey))]
 pub fn model_derive(input: TokenStream) -> TokenStream {
     let mut ast = parse_macro_input!(input as DeriveInput);
     let model = match parse_model(&mut ast) {
