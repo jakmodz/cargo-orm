9ac1468e713d9d2fe66647e86dbc0456bceeefa3
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-02-27 22:27:03 +0100

    docs: documentation for GenerationType enum

--- crates/cargo-orm-core/src/types/generation_strategy.rs
+++ crates/cargo-orm-core/src/types/generation_strategy.rs
@@ -1,4 +1,6 @@
 #[derive(deluxe::ParseMetaItem, Debug, PartialEq, Eq)]
+/// Represents the generation strategy for a column.
 pub enum GenerationType {
+    /// Automatically generates a unique identifier for each row.
     AutoIncrement,
 }
