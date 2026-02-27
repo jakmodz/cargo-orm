33424e730244c16190e5c2b4f0139c863a120ac6
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-02-27 22:27:32 +0100

    feat: more Data types for SqlType

--- crates/cargo-orm-core/src/types/colum_type.rs
+++ crates/cargo-orm-core/src/types/colum_type.rs
@@ -1,13 +1,25 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SqlType {
+    /// Represents an integer value.
     Integer,
+    /// Represents a decimal value.
+    Float,
+    /// Represents a double value.
+    Double,
+    /// Represents a character value with a fixed length.
+    Char(usize),
+    /// Represents a string value with a maximum length.
     Varchar(usize),
+    /// Represents a text value with no maximum length.
     Text,
+    /// Represents a boolean value.
     Boolean,
+    /// Represents a date value.
     Date,
+    /// Represents a timestamp value.
     Timestamp,
 }
-
+/// Trait for casting rust types to sql types
 pub trait ToSqlType {
     fn to_sql_type(&self) -> SqlType;
 }
@@ -40,7 +52,56 @@ impl ToSqlType for chrono::NaiveDateTime {
         SqlType::Timestamp
     }
 }
-
+impl ToSqlType for u16 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for u32 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for u64 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for i64 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for u8 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for i8 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for u128 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for i128 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Integer
+    }
+}
+impl ToSqlType for f32 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Float
+    }
+}
+impl ToSqlType for f64 {
+    fn to_sql_type(&self) -> SqlType {
+        SqlType::Double
+    }
+}
 impl<T: ToSqlType + Default> ToSqlType for Option<T> {
     fn to_sql_type(&self) -> SqlType {
         T::default().to_sql_type()
