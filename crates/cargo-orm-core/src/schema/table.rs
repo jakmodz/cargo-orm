07ec10b0ddd637e9c1315bb95326c631b295c9b2
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-02-28 22:27:34 +0100

    docs: short descriptions of structs and fields and methods

--- crates/cargo-orm-core/src/schema/table.rs
+++ crates/cargo-orm-core/src/schema/table.rs
@@ -2,31 +2,48 @@ use crate::types::{
     colum_type::{SqlType, ToSqlType},
     generation_strategy::GenerationType,
 };
+
 pub trait TableSchema {
+    /// Method to get the table name
     fn get_table_name() -> &'static str;
+    /// Method to get the table schema
     fn get_schema() -> TableSchemaModel;
 }
-
+/// Struct representing the table model
 pub struct TableSchemaModel {
+    /// Name of the table
     pub name: String,
+    /// Fields of the table
     pub fields: Vec<ColumnSchemaModel>,
+    /// Indexes of the table
     pub indexes: Vec<IndexModel>,
+    /// Primary key of the table
     pub primary_key: PrimaryKeyModel,
 }
-
+/// Struct representing the column schema model
 pub struct ColumnSchemaModel {
+    /// Name of the column
     pub name: String,
+    /// Whether the column is nullable or not
     pub is_nullable: bool,
+    /// Type of the column
     pub sql_type: SqlType,
 }
 pub struct IndexModel {
+    /// Name of the index
     pub name: String,
+    /// Fields of the index
     pub fields: Vec<String>,
+    /// Whether the index is unique or not
     pub unique: bool,
 }
+/// Struct representing the primary key model
 pub struct PrimaryKeyModel {
+    /// Name of the primary key
     pub name: String,
+    /// Generation type of the primary key
     pub generation_type: Option<GenerationType>,
+    /// Type of the primary key
     pub ty: SqlType,
 }
 impl ColumnSchemaModel {
