e945396192ea76a1a8cdd72517bbe8ee91f22607
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-03-07 15:51:03 +0100

    feat: sqlite dialect with type_casting and tests

---
+++ crates/cargo-orm-core/src/dialect/sqlite_dialect/sqlite.rs
@@ -0,0 +1,54 @@
+use crate::{dialect::sql_dialect::SqlDialect, types::column_type::SqlType};
+
+pub struct SqliteDialect;
+
+impl SqlDialect for SqliteDialect {
+    fn cast_type(&self,sql_type: &crate::types::column_type::SqlType) -> String {
+        match sql_type {
+            SqlType::Integer => "INTEGER",
+            SqlType::Float => "REAL",
+            SqlType::Double => "REAL",
+            SqlType::Char(_) => "TEXT",
+            SqlType::Varchar(_) => "TEXT",
+            SqlType::Text => "TEXT",
+            SqlType::Boolean => "INTEGER",
+            SqlType::Date => "TEXT",
+            SqlType::Timestamp => "TEXT",
+            SqlType::Custom(_) => unreachable!()//TODO,
+        }.to_string()   
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const DIALECT: SqliteDialect = SqliteDialect;
+    #[test]
+    fn test_cast_type_integer() {
+        
+        assert_eq!(DIALECT.cast_type(&SqlType::Integer), "INTEGER");
+    }
+    #[test]
+    fn test_cast_type_float() {
+        assert_eq!(DIALECT.cast_type(&SqlType::Float), "REAL");
+    }
+    #[test]
+    fn test_cast_type_double() {
+        assert_eq!(DIALECT.cast_type(&SqlType::Double), "REAL");
+    }
+    #[test]
+    fn test_cast_type_char() {
+        
+        assert_eq!(DIALECT.cast_type(&SqlType::Char(10)), "TEXT");
+    }
+    #[test]
+    fn test_cast_type_varchar() {
+        
+        assert_eq!(DIALECT.cast_type(&SqlType::Varchar(10)), "TEXT");
+    }
+    #[test]
+    fn test_cast_type_text() {
+        
+        assert_eq!(DIALECT.cast_type(&SqlType::Text), "TEXT");
+    }    
+}
