f0093c1e89c446c0c6a978a6171abb0a6ee1b6ce
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-03-10 13:20:23 +0100

    fix: Self in return for save() method of repository + pub keyword for trait

--- crates/cargo-orm-core/src/model/repository.rs
+++ crates/cargo-orm-core/src/model/repository.rs
@@ -1,10 +1,10 @@
 use crate::{driver::sql_driver::SqlDriver, error::CargoOrmError};
 
-trait Repository: Sized + Sync {
+pub trait Repository: Sized + Sync {
     type PrimaryKey;
     type Db: SqlDriver;
 
-    async fn save<'db>(&self, db: &'db Self::Db) -> Result<(), CargoOrmError>;
+    async fn save<'db>(&self, db: &'db Self::Db) -> Result<Self, CargoOrmError>;
     async fn get_all<'db>(db: &'db Self::Db) -> Result<Vec<Self>, CargoOrmError>;
     async fn get_by_id<'db>(id: Self::PrimaryKey, db: &'db Self::Db)
     -> Result<Self, CargoOrmError>;
