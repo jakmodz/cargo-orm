0c469b53f2ca2e769d7a4f4e93f4fa40d0e69426
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-03-27 14:47:40 +0100

    fix: Change repository method in test from get_by_id() to save()

--- crates/cargo-orm-test/src/repository_test/save_test.rs
+++ crates/cargo-orm-test/src/repository_test/save_test.rs
@@ -12,6 +12,6 @@ mod tests {
         let driver = init_sqlite().await;
         let mut conn = driver.acquire_conn().await.unwrap();
         user.save(&mut conn).await.unwrap();
-        let _saved_user = User::get_by_id(1, &mut conn).await.unwrap();
+        let _saved_user = user.save(&mut conn).await.unwrap();
     }
 }
