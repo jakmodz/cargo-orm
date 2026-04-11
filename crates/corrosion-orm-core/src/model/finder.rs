2609ecc22600f24edbc189125594f7ba849864c0
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-04-11 18:27:00 +0200

    docs: Add docs to Finder methods

--- crates/corrosion-orm-core/src/model/finder.rs
+++ crates/corrosion-orm-core/src/model/finder.rs
@@ -28,6 +28,9 @@ where
             _executor: PhantomData,
         }
     }
+    /// Limits the number of rows returned by the query.
+    ///
+    /// Every call to this method replaces the previous limit.
     pub fn limit(self, limit: usize) -> Self {
         Self {
             query: self.query.limit(limit),
@@ -35,6 +38,9 @@ where
             _executor: PhantomData,
         }
     }
+    /// Filters the query using the given [`WhereClause`].
+    ///
+    /// Every call to this method replaces the previous filter.
     pub fn filter(self, filter: WhereClause<'query>) -> Self {
         Self {
             query: self.query.where_clause(filter),
@@ -42,12 +48,18 @@ where
             _executor: PhantomData,
         }
     }
+    /// Fetches a single row from the query.
+    ///
+    /// Returns `Err(CorrosionOrmError::DriverError())` if row is not found.
     pub async fn one(self, executor: &mut E) -> Result<T, CorrosionOrmError> {
         let mut ctx = QueryContext::new();
         self.query.to_sql(&mut ctx, executor.get_dialect());
         let res = executor.fetch_one(&mut ctx).await?;
         Ok(res)
     }
+    /// Fetches all rows from the query.
+    ///
+    /// Returns an empty vector if no rows are found.
     pub async fn all(self, executor: &mut E) -> Result<Vec<T>, CorrosionOrmError> {
         let mut ctx = QueryContext::new();
         self.query.to_sql(&mut ctx, executor.get_dialect());
